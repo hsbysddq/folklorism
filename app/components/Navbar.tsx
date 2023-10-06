f4ce96a06411427519c8f33ec181254b39c9f653
hsbysddq <hsby15sddq@gmail.com>
2023-10-07 06:11:26 +0700

    Edit navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image"
+import { FaRegBell } from "react-icons/fa"
 const Navbar = () => {
   return (
-    <div className="container">
-      <div className="flex items-center py-4">
-        <Image
-          src="/avatar.png"
-          alt=""
-          width={40}
-          height={40}
-          className="rounded-full"
-        />
-        <p className="font-semibold pl-3">Hi, Hasbi Siddiqi</p>
+    <div className="bg-transparent">
+      <div className="container flex items-center py-6 justify-between">
+        <div className="flex items-center">
+          <Image
+            src="/avatar.png"
+            alt=""
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+          <p className="font-semibold pl-3">Hi, Hasbi Siddiqi</p>
+        </div>
+        <FaRegBell className="w-6 h-6" />
       </div>
     </div>
   )
