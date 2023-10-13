6413dead78c9e3e2c52f39a13968ad191171f88e
hsbysddq <hsby15sddq@gmail.com>
2023-10-14 00:52:56 +0700

    edit navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image"
 import { TiThMenu } from "react-icons/ti"
+import { MdWavingHand } from "react-icons/md"
 const Navbar = () => {
   return (
     <div className="bg-transparent">
-      <div className="container flex items-center py-6 justify-between">
+      <div className="container flex items-center py-5 justify-between">
         <div className="flex items-center">
           <Image
             src="/avatar.png"
@@ -12,13 +13,16 @@ const Navbar = () => {
             height={50}
             className="rounded-full"
           />
-          <div className="font-semibold pl-3 text-[#F3DECC]">
+          <div className="font-semibold pl-3 text-[#983820]">
             <p className="text-sm">Hello</p>
-            <p className="text-base">Hasbi Siddiqi</p>
+            <div className="flex">
+              <p className="text-base mr-2">Hasbi Siddiqi</p>
+              <MdWavingHand className="text-amber-500" />
+            </div>
           </div>
         </div>
-        <div className="flex justify-center items-center w-8 h-8 bg-[#F3DECC] rounded-lg">
-          <TiThMenu className="w-5 h-5 text-[#983820]" />
+        <div className="flex justify-center items-center w-8 h-8 bg-[#DAB19D] rounded-lg">
+          <TiThMenu className="w-5 h-5 text-[#F3DECC]" />
         </div>
       </div>
     </div>
