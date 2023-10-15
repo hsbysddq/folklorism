dfe1ca819173810482da15970432d51e5adb621a
hsbysddq <hsby15sddq@gmail.com>
2023-10-15 14:15:33 +0700

    Edit Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { MdWavingHand } from "react-icons/md"
 const Navbar = () => {
   return (
     <div className="bg-transparent">
-      <div className="container flex items-center py-5 justify-between">
+      <div className="md:hidden container flex items-center py-5 justify-between">
         <div className="flex items-center">
           <Image
             src="/avatar.png"
@@ -13,7 +13,7 @@ const Navbar = () => {
             height={50}
             className="rounded-full"
           />
-          <div className="font-semibold pl-3 text-[#983820]">
+          <div className="font-semibold pl-3 text-[#F4DFBA]">
             <p className="text-sm">Hello</p>
             <div className="flex">
               <p className="text-base mr-2">Hasbi Siddiqi</p>
@@ -22,7 +22,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="flex justify-center items-center w-8 h-8 bg-[#DAB19D] rounded-lg">
-          <TiThMenu className="w-5 h-5 text-[#F3DECC]" />
+          <TiThMenu className="w-5 h-5 text-[#F4DFBA]" />
         </div>
       </div>
     </div>
