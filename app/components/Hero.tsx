0c69d88a7f7bab8fe37f287acd0218449875b40d
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-10-26 01:07:43 +0700

    Edit hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,19 +1,27 @@
 import React from "react"
-import Navbar from "./Navbar"
+
 import SearchForm from "./SearchForm"
 import Category from "./Category/Category"
 import ProgressPoint from "./ProgressPoint"
 import Tagline from "./Tagline"
+import Navbar from "./Navbar/Navbar"
+import Nav from "./Nav"
+import ProgressCourse from "./ProgressCourse"
 
 const Hero = () => {
+  const user = true
+
   return (
     <div className="bg-[#FEF7C2]">
-      <Navbar />
-      <div className="container">
-        <Tagline />
+      {!user ? <Navbar /> : <Nav />}
+
+      <div className="container pt-6">
+        {!user ? <Tagline /> : <ProgressPoint />}
+
         {/* <ProgressPoint /> */}
         <SearchForm />
       </div>
+      {/* <ProgressCourse /> */}
       <Category />
     </div>
   )
