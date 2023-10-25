f16dab375e0360d0646aed69cf0609ca76cda980
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-10-26 02:24:03 +0700

    Edit navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -8,12 +8,12 @@ const Navbar = () => {
   const user = false
 
   return (
-    <div className="h-12 text-[#983820] p-4 flex items-center justify-between border-b-2 border-[#983820] bg-[#FEF7C2] uppercase md:24 lg:px-20 xl:px-40">
+    <div className="font-montserrat h-20 md:h-16 text-[#983820] p-4 flex items-center justify-between border-b-2 border-[#983820] bg-transparent uppercase md:24 lg:px-20 xl:px-40">
       {/* LEFT LINK */}
-      <div className="hidden md:flex gap-4 flex-1">
+      <div className="hidden md:flex gap-4 flex-1 md:font-semibold md:gap-8 md:my-1">
         <Link href="/">Homepage</Link>
-        <Link href="/menu">Menu</Link>
-        <Link href="/">Contact</Link>
+        <Link href="/menu">Media</Link>
+        <Link href="/">Event</Link>
       </div>
       {/* LOGO */}
       <div className="text-xl md:font-bold flex-1 md:text-center font-bold font-montserrat">
@@ -24,16 +24,10 @@ const Navbar = () => {
         <Menu />
       </div>
       {/* RIGHT LINK */}
-      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
-        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
-          <Image src="/phone.png" alt="" width={20} height={20}></Image>
-          <span>+62 838 341</span>
-        </div>
-        {!user ? (
-          <Link href="/">Login</Link>
-        ) : (
-          <Link href="/orders">Orders</Link>
-        )}
+      <div className="hidden md:flex gap-4 md:gap-8 md:font-semibold items-center justify-end flex-1">
+        <Link href="/">Comunity</Link>
+
+        <Link href="/orders">Showcase</Link>
       </div>
     </div>
   )
