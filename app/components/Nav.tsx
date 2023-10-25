a0e864ed933f8857ae6cd62e8a3c75e4e0dad698
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-10-26 01:07:58 +0700

    Edit nav

---
+++ app/components/Nav.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import { MdWavingHand } from "react-icons/md"
+import Menu from "./Navbar/Menu"
+
+const Nav = () => {
+  return (
+    <div className="container">
+      <div className="bg-transparent">
+        {/* NAVBAR LOGIN */}
+        <div className="flex items-center pt-6 justify-between">
+          <div className="flex items-center">
+            <div className="flex">
+              <Image
+                src="/avatar.png"
+                alt=""
+                width={50}
+                height={50}
+                className="rounded-full"
+              />
+              <div className="font-semibold pl-3 text-[#983820]">
+                <p className="text-sm">Hello</p>
+                <div className="flex">
+                  <p className="text-base mr-2">Hasbi Siddiqi</p>
+                  <MdWavingHand className="text-amber-500" />
+                </div>
+              </div>
+            </div>
+
+            <div className="text-[#983820] ml-[136px] md:hidden">
+              <Menu />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
