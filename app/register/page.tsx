811c97136625bca8b990233b49def8d20c65ce1b
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-10-26 22:30:08 +0700

    edit register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,13 +1,13 @@
 import React from "react"
-import Link from "next/link"
 import Navbar from "../components/Navbar/Navbar"
+import Link from "next/link"
 
 const RegisterPage = () => {
   return (
     <div className="bg-[#FEF7C2] overflow-hidden">
       <Navbar />
       <div className="bg-[#FEF7C2] w-screen h-[calc(100vh-4rem)] flex items-center">
-        <div className="w-[350px] bg-[#F4DFBA] rounded-xl m-5">
+        <div className="md:hidden w-[350px] bg-[#F4DFBA] rounded-xl m-5">
           <div className="">
             <h1 className="text-center font-extrabold text-3xl text-[#983820] pt-10">
               Register
@@ -15,17 +15,18 @@ const RegisterPage = () => {
             <form action="" className="mt-6 flex flex-col items-center">
               <input
                 type="text"
-                name="username"
-                placeholder="Username"
+                name="fullName"
+                placeholder="Full Name"
                 className="w-10/12  mt-4 px-4 py-5 rounded-2xl  bg-slate-50 placeholder-[#983820] font-semibold"
               />
 
               <input
-                type="email"
-                name="email"
+                type="text"
+                name="username"
                 placeholder="Email"
                 className="w-10/12  mt-4 px-4 py-5 rounded-2xl  bg-slate-50 placeholder-[#983820] font-semibold"
               />
+
               <input
                 type="password"
                 name="password"
@@ -34,18 +35,62 @@ const RegisterPage = () => {
               />
             </form>
             <div className="pt-4 px-8">
-              <div className="flex">
-                <p className="pb-8 text-[#983820] font-bold mr-2">
-                  Do you have account ?
+              <p className="pb-8 text-[#983820] font-bold">Forgot Password ?</p>
+              <Link href="/login">
+                <button className="font-bold text-lg bg-[#983820] text-[#DAB19D] p-4 w-full rounded-2xl mb-8">
+                  Register
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* DESKTOP */}
+        <div className="hidden md:flex md:bg-[#F4DFBA] md:mx-56 md:w-8/12 md:h-[70%] md:rounded-[40px]">
+          <div className="hidden md:flex w-full">
+            <div className="flex flex-col w-6/12">
+              <h1 className="text-center font-extrabold text-3xl text-[#983820] pt-10">
+                Login
+              </h1>
+              <form action="" className="mt-6 flex flex-col items-center">
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Email"
+                  className="w-10/12  mt-4 px-4 py-5 rounded-2xl  bg-slate-50 placeholder-[#983820] font-semibold"
+                />
+
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="w-10/12 bg-slate-50 placeholder-[#983820] font-semibold mt-4 px-4 py-5 rounded-2xl"
+                />
+              </form>
+              <div className="pt-4 px-8">
+                <p className="pb-8 text-[#983820] font-bold">
+                  Forgot Password ?
                 </p>
-                <Link href="/" className="text-[#983820] font-bold">
-                  Login
+                <Link href="/login">
+                  <button className="font-bold text-lg bg-[#983820] text-[#DAB19D] p-4 w-full rounded-2xl mb-8">
+                    Register
+                  </button>
                 </Link>
               </div>
+            </div>
 
-              <button className="font-bold text-lg bg-[#983820] text-[#DAB19D] p-4 w-full rounded-2xl mb-8">
-                Register
-              </button>
+            <div className="flex flex-col items-center bg-[#983820] rounded-[40px] rounded-s-[110px] justify-center">
+              <p className="font-montserrat font-bold text-3xl text-[#dbac94]">
+                Hello, Friend!
+              </p>
+              <p className="w-10/12 py-2 text-center font-medium font-montserrat text-[#dbac94] mb-2">
+                Register with your personal details to use all of site features
+              </p>
+              <Link href="/login">
+                <button className="font-bold text-lg bg-[#e6ccbe]  text-[#983820] p-4 w-4/12 rounded-2xl mb-8">
+                  Register
+                </button>
+              </Link>
             </div>
           </div>
         </div>
