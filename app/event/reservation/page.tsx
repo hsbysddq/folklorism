2db3cf0437b337be8018fc380da9ce0b10604cac
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-11-02 22:14:44 +0700

    edit reservation

--- app/event/reservation/page.tsx
+++ app/event/reservation/page.tsx
@@ -1,17 +1,11 @@
+import Reservation from "@/app/components/Event/Reservation/Reservation"
 import Image from "next/image"
 import React from "react"
 
 const ReservationPage = () => {
   return (
-    <div className="h-full w-full">
-      {/* IMAGE */}
-      <div className="h-1/2 w-full">
-        <Image src="/avatar.png" alt="" fill className=""></Image>
-      </div>
-      {/* Card */}
-      <div className="h-1/2 w-full">
-        <h1>jkhsdafgl</h1>
-      </div>
+    <div className="">
+      <Reservation />
     </div>
   )
 }
