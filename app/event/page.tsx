d8e28acd9151dfb0a8abac60ee4a3462e06db281
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-11-03 12:52:06 +0700

    edit login page

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import Event from "../components/Event/Event"
 const Loginpage = () => {
   return (
-    <div className="bg-[#F4DFBA]">
+    <div className="bg-[#F4DFBA] w-full h-screen">
       <div>
         <h1>Hi, Hasbi</h1>
       </div>
