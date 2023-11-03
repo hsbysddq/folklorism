1afac249e2cf1bc2b4f4b5d77188ca5d6338c5ee
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-11-04 02:13:03 +0700

    edit

--- app/components/Category/Card.tsx
+++ app/components/Category/Card.tsx
@@ -31,10 +31,9 @@ const Card = () => {
   return (
     <div className="flex flex-col md:flex-row xl:flex-row items-center pb-2">
       {course.map((video) => (
-        <div className="w-11/12 md:ml-7 xl:ml-10 mb-7">
+        <div key={video.id} className="w-11/12 md:ml-7 xl:ml-10 mb-7">
           <div className="h-2 bg-transparent">
             <img
-              key={video.id}
               src={video.url}
               alt=""
               className="w-full h-[210px] bg-contain rounded-t-3xl"
@@ -42,15 +41,9 @@ const Card = () => {
           </div>
           <div className="bg-slate-50 rounded-b-3xl mt-[200px] pb-2 font-montserrat flex flex-col">
             <div className="font-montserrat px-3 pt-4 text-[#983820] flex flex-col">
-              <p key={video.id} className="font-semibold">
-                {video.title}
-              </p>
-              <p key={video.id} className="pt-3 font-medium pb-2">
-                {video.chapter}
-              </p>
-              <p key={video.id} className="font-medium pb-2">
-                {video.duration}
-              </p>
+              <p className="font-semibold">{video.title}</p>
+              <p className="pt-3 font-medium pb-2">{video.chapter}</p>
+              <p className="font-medium pb-2">{video.duration}</p>
               <button className="font-extrabold text-lg bg-[#e6ccbe]  text-[#983820] p-2 w-4/12 rounded-2xl mb-2 ml-52">
                 Start
               </button>
