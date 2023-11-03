80ccc2478ab1d74cf0e099c3e48f17af3f2393a3
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-11-03 12:51:07 +0700

    Edit footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,17 +4,8 @@ const Footer = () => {
   return (
     <div className="max-w-screen-2xl mx-auto">
       <footer className="p-4 bg-[#FEF7C2] rounded-lg shadow md:px-6 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="#"
-            target="_blank"
-            className="flex items-center mb-4 sm:mb-0"
-          >
-            <span className="self-center text-xl font-semibold whitespace-nowrap text-[#983820]">
-              Folkrism
-            </span>
-          </a>
-          <ul className="flex flex-wrap items-center mb-6 sm:mb-0 text-[#983820]">
+        <div className="">
+          <ul className="flex flex-wrap items-center justify-center mb-6 sm:mb-0 text-[#983820]">
             <li>
               <a
                 href="#"
@@ -41,12 +32,12 @@ const Footer = () => {
           </ul>
         </div>
         <hr className="my-6 sm:mx-auto border-[#983820] lg:my-8" />
-        <span className="block text-sm text-[#983820] sm:text-center ">
-          © 2022{" "}
+        <span className="flex justify-center text-sm text-[#983820] sm:text-center ">
+          © 2023
           <a
             href="https://flowbite.com"
             target="_blank"
-            className="hover:underline"
+            className="hover:underline mx-2"
           >
             Folkrism™
           </a>
